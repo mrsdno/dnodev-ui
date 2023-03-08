13993708daf01405ee74e3d9c873baf9f62272f6
mrsdno <christine@dno.dev>
2023-03-07 20:16:51 -0500

    move header to reusable component and set up peloton page

--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,10 @@ router.get('/dailyplanner', function (req, res, next) {
   res.render("dailyplanner", { title: 'A daily planner by Christine DNO' });
 })
 
+/* GET peloton tracker */
+router.get('/peloton', function (req, res, next) {
+  res.render("peloton", { title: 'A peloton tracker by Christine DNO' });
+})
+
 module.exports = router;
 
